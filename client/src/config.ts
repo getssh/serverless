3b74469477d0d17a3ef5a573d93521dac3b5f4ab
getssh <gtamrat33@gmail.com>
2023-02-16 18:56:30 +0300

    Add auth0 doamin and api endpoint to config.ts

--- client/src/config.ts
+++ client/src/config.ts
@@ -1,11 +1,11 @@
 // TODO: Once your application is deployed, copy an API id here so that the frontend could interact with it
-const apiId = '...'
+const apiId = 'vpkhia9m46'
 export const apiEndpoint = `https://${apiId}.execute-api.us-east-1.amazonaws.com/dev`
 
 export const authConfig = {
   // TODO: Create an Auth0 application and copy values from it into this map. For example:
   // domain: 'dev-nd9990-p4.us.auth0.com',
-  domain: '...',            // Auth0 domain
-  clientId: '...',          // Auth0 client id
+  domain: 'dev-ugskd5kg25m601si.us.auth0.com',            // Auth0 domain
+  clientId: 'GskNEoC3wWNcaXHdSpuikxDd0ps1LqVb',          // Auth0 client id
   callbackUrl: 'http://localhost:3000/callback'
 }
