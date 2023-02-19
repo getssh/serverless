e9f154640531eeba94713613714a2e92e51c17e9
getssh <gtamrat33@gmail.com>
2023-02-19 13:28:31 +0300

    Edit auth and busines logic

--- backend/src/helpers/attachmentUtils.ts
+++ backend/src/helpers/attachmentUtils.ts
@@ -4,25 +4,24 @@ import * as AWSXRay from 'aws-xray-sdk'
 const XAWS = AWSXRay.captureAWS(AWS)
 
 // TODO: Implement the fileStogare logic
-
 const s3BucketName = process.env.ATTACHMENT_S3_BUCKET
-const urlExpiration = process.env.SIGNED_URL_EXPIRATION
+const urlExpiration = 300
 
-export class AttachmentUtils{
-  constructor (
-    private readonly s3 = new XAWS.S3({ signatureVersion : 'v4' }),
-    private readonly bucketName = s3BucketName
-  ) {} 
-  getAttachmentUrl(todoId : string){
-     return `https://${this.bucketName}.s3.amazonaws.com/${todoId}`
-  }
+export class AttachmentUtils {
+    constructor(
+        private readonly s3 = new XAWS.S3({ signatureVersion: 'v4' }),
+        private readonly bucketName = s3BucketName) {}
 
-  getUploadUrl(todoId: string): string{
-    const url = this.s3.getSignedUrl('putObject', {
-      Bucket: this.bucketName,
-      Key: todoId,
-      Expires: urlExpiration
-    })
-    return url as string
-  }
+    getAttachmentUrl(todoId: string) {
+        return `https://${this.bucketName}.s3.amazonaws.com/${todoId}`
+    }
+
+    getUploadUrl(todoId: string): string {
+        const url = this.s3.getSignedUrl('putObject', {
+            Bucket: this.bucketName,
+            Key: todoId,
+            Expires: urlExpiration
+        })
+        return url as string
+    }
 }
